84bb6f8dd81f058902e82b499abb2610769a5220
sqzume <173809390+sqzume@users.noreply.github.com>
2026-02-11 19:08:18 +0900

    コンテストIDから問題URLを出力する機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use scraper::{Html, Selector};
+use std::env;
+
+fn get_problem_urls(html: &str) -> Vec<String> {
+    let document = Html::parse_document(&html);
+    let selector = Selector::parse("td.text-center a").unwrap();
+
+    document
+        .select(&selector)
+        .filter_map(|element| {
+            let href = element.value().attr("href")?;
+            if href.contains("/tasks/") {
+                Some(format!("https://atcoder.jp{}", href))
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+fn get(contest_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let contest_url = format!("https://atcoder.jp/contests/{}/tasks", contest_name);
+    let body = reqwest::blocking::get(contest_url)?.text()?;
+    let urls = get_problem_urls(&body);
+    for x in urls {
+        println!("{}", x);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("コンテスト名を入力してください");
+        return;
+    }
+    get(&args[1]).expect("取得に失敗しました");
 }
