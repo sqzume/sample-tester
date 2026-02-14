156a52daf9851a4859ef81b45ddd5c67f85296d2
sqzume <173809390+sqzume@users.noreply.github.com>
2026-02-14 20:45:06 +0900

    main.rs にコメントを追加

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use scraper::{Html, Selector};
 use std::env;
 
+fn get_sample_case(url: &str) {}
+
 fn get_problem_urls(html: &str) -> Vec<String> {
     let document = Html::parse_document(&html);
+    // URL が書かれている a タグの class を指定
     let selector = Selector::parse("td.text-center a").unwrap();
 
     document
@@ -20,7 +23,9 @@ fn get_problem_urls(html: &str) -> Vec<String> {
 
 fn get(contest_name: &str) -> Result<(), Box<dyn std::error::Error>> {
     let contest_url = format!("https://atcoder.jp/contests/{}/tasks", contest_name);
+    // コンテストの問題一覧ページの HTML を取得
     let body = reqwest::blocking::get(contest_url)?.text()?;
+    // 問題の URL を配列で受け取る
     let urls = get_problem_urls(&body);
     for x in urls {
         println!("{}", x);
