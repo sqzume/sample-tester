fcf7442a2bfbb8acfb74403939523272aaf7e79e
sqzume <173809390+sqzume@users.noreply.github.com>
2026-02-17 22:59:27 +0900

    関数のリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,16 @@ fn get_sample_case(url: &str) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn get_problem_urls(html: &str) -> Vec<String> {
+fn get_problem_urls(contest_name: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let contest_url = format!("https://atcoder.jp/contests/{}/tasks", contest_name);
+    // コンテストの問題一覧ページの HTML を取得
+    let html = reqwest::blocking::get(contest_url)?.text()?;
+
     let document = Html::parse_document(&html);
     // URL が書かれている a タグの class を指定
     let selector = Selector::parse("td.text-center a").unwrap();
 
-    document
+    let urls: Vec<String> = document
         .select(&selector)
         .filter_map(|element| {
             let href = element.value().attr("href")?;
@@ -51,27 +55,21 @@ fn get_problem_urls(html: &str) -> Vec<String> {
                 None
             }
         })
-        .collect()
+        .collect::<Vec<String>>();
+
+    Ok(urls)
 }
 
-fn get(contest_name: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let contest_url = format!("https://atcoder.jp/contests/{}/tasks", contest_name);
-    // コンテストの問題一覧ページの HTML を取得
-    let body = reqwest::blocking::get(contest_url)?.text()?;
-    // 問題の URL を配列で受け取る
-    let urls = get_problem_urls(&body);
-    for x in urls {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("コンテスト名を入力してください");
+        return Ok(());
+    }
+    let urls: Vec<String> = get_problem_urls(&args[1])?;
+    for x in &urls {
         println!("{}", x);
         get_sample_case(&x).expect("サンプルケースの取得に失敗しました");
     }
     Ok(())
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("コンテスト名を入力してください");
-        return;
-    }
-    get(&args[1]).expect("取得に失敗しました");
 }
